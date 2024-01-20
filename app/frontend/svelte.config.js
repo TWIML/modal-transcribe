5453415b9f3e7405271a829561be2232ae17e951
Sam Charrington <sam@SBCMBP.local>
2024-01-20 17:40:38 -0300

    Add SPA fallback

--- app/frontend/svelte.config.js
+++ app/frontend/svelte.config.js
@@ -12,7 +12,8 @@ const config = {
 		// See https://kit.svelte.dev/docs#adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'dist',
-			fallback: null,
+			assets: 'dist',
+			fallback: 'index.html',
 			strict: false
 		})
 	}
